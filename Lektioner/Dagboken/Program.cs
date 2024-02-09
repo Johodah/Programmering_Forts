d0898dd8d7e8a8cbf89a381d1a4939be728769cc
Johohoa <johnny.dahle@hotmail.com>
2024-02-09 11:02:16 +0100

    Lekt med metoder i andra lektionen

---
+++ Lektioner/Dagboken/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hej, vad vill du vända på? ");
+        string input = Console.ReadLine();
+
+       
+        var NewString = ReverseString(input);  
+        Console.WriteLine(NewString);
+
+        Console.Write("Dags att kalkylera. Ge mig två tal att plussa, ett i taget: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("and another one");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        int total = CalculateProduct(n1, n2);
+        Console.WriteLine("Summan av siffrorna blir: " + total);
+
+        Console.WriteLine("Nu ska vi räkna kvadratrot, ge mig ett nummer att finurla på: ");
+        double R1 = Convert.ToDouble(Console.ReadLine());
+        double R2 = CalculateRoot(R1);
+        Console.WriteLine("Kvadratroten av: " + R1 + " blir " + R2);
+        
+        
+    }
+
+    public static string ReverseString(string StringToReverse)
+    {
+        var charArray = StringToReverse.ToCharArray();
+        Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+    public static int CalculateProduct(int n1, int n2)
+    {
+        int total;
+        total = n1 + n2;
+        return total;
+    }
+
+    public static double CalculateRoot(double R1)
+    {
+        double R2 = Math.Sqrt(R1);
+        return R2;
+    }
+}
+
--- Lektioner/Warmup/Program.cs
+++ Lektioner/Warmup/Program.cs
@@ -1,19 +1,128 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Random random = new Random();
+﻿using System;
 
-        int FirstDice = random.Next (1, 6);
-        int SecondDice = random.Next(1, 6);
-        if ( FirstDice == SecondDice )
+public class Entity
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Symbol { get; set; }
+
+    public Entity(int x, int y, char symbol)
+    {
+        X = x;
+        Y = y;
+        Symbol = symbol;
+    }
+
+    public virtual void Draw()
+    {
+        Console.SetCursorPosition(X, Y);
+        Console.Write(Symbol);
+    }
+}
+
+public class Player : Entity
+{
+    public Player(int x, int y) : base(x, y, 'P')
+    {
+    }
+
+    public void Move(int dx, int dy)
+    {
+        // Implementera rörelsebegränsningar här om det behövs
+        X += dx;
+        Y += dy;
+    }
+
+    public void Interact(Entity entity)
+    {
+        // Implementera interaktionslogik här
+        if (entity is Creature)
         {
-            Console.WriteLine("Du fick två stycken " + FirstDice);
+            Creature creature = (Creature)entity;
+            // Implementera möteslogik mellan spelaren och varelsen här
+            Console.WriteLine($"Du mötte en varelse av typen {creature.Symbol}!");
         }
-        else
+        else if (entity is Item)
         {
-            Console.WriteLine($"Du fick: " + FirstDice + " och " + SecondDice);
+            Item item = (Item)entity;
+            // Implementera logik för att plocka upp föremål här
+            Console.WriteLine($"Du plockade upp ett föremål av typen {item.Symbol}!");
         }
-       
+    }
+}
+
+public class Creature : Entity
+{
+    public Creature(int x, int y, char symbol) : base(x, y, symbol)
+    {
+    }
+}
+
+public class Item : Entity
+{
+    public bool Consumable { get; set; }
+    public bool Equippable { get; set; }
+
+    public Item(int x, int y, char symbol, bool consumable, bool equippable) : base(x, y, symbol)
+    {
+        Consumable = consumable;
+        Equippable = equippable;
+    }
+
+    public void Use(Player player)
+    {
+        // Implementera effekterna av att använda föremålet här
+        if (Consumable)
+        {
+            Console.WriteLine($"Du använder det förbrukningsbara föremålet {Symbol}.");
+            // Implementera effekter för förbrukningsbara föremål här
+        }
+        else if (Equippable)
+        {
+            Console.WriteLine($"Du sätter på dig det bärbara föremålet {Symbol}.");
+            // Implementera effekter för bärbara föremål här
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player(0, 0);
+        Creature creature = new Creature(5, 5, 'M');
+        Item item = new Item(10, 10, 'H', true, false); // Exempel på ett förbrukningsbart föremål
+
+        while (true)
+        {
+            Console.Clear();
+            player.Draw();
+            creature.Draw();
+            item.Draw();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    player.Move(0, -1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    player.Move(0, 1);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    player.Move(-1, 0);
+                    break;
+                case ConsoleKey.RightArrow:
+                    player.Move(1, 0);
+                    break;
+                case ConsoleKey.E:
+                    player.Interact(creature);
+                    break;
+                case ConsoleKey.Spacebar:
+                    player.Interact(item);
+                    item.Use(player);
+                    break;
+            }
+        }
     }
 }
