f5bde0dcca1d8b3970c4c8b8c550cc7c03976b13
Johohoa <johnny.dahle@hotmail.com>
2024-02-12 08:35:10 +0100

    Satt upp labbmappen och solution

---
+++ Lektioner/Dagbok/DiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dagbok
+{
+    public class DiaryEntry
+    {
+        public DateTime datum;
+
+        public string rubrik;
+
+        public string innehåll;
+    }
+
+    public DiaryEntry(DateTime datum1, string rubrik, string innehåll)
+    {
+        Datum1 = datum1;
+        this.Rubrik = rubrik;
+        this.Innehåll = innehåll;
+    }
+
+    //public DateTime Datum
+    //{
+    //    get { return datum; }
+    //    set { datum = value; }
+    //}
+    public string Rubrik
+    {
+        get { return rubrik; }
+        set { rubrik = value; }
+    }
+    public string Innehåll
+    {
+        get { return innehåll; }
+        set { innehåll = value; }
+    }
+
+    public DateTime Datum1
+    {
+        get;
+    }
+
+}
---
+++ Lektioner/Dagbok/Program.cs
@@ -0,0 +1,55 @@
+﻿using DiaryEntry;
+
+class Program
+{
+    // Implementera metoderna som beskrivs ovan
+    // ...
+
+    static void Main(string[] args)
+    {
+        List<DiaryEntry> diaryEntries = new List<DiaryEntry>();
+
+        // Huvudmenyloop
+        while (true)
+        {
+            Console.WriteLine("1. Läs dagboksinlägg");
+            Console.WriteLine("2. Skapa nytt dagboksinlägg");
+            Console.WriteLine("3. Avsluta");
+
+            Console.Write("Välj ett alternativ: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            switch (val)
+            {
+                case 1:
+                    ReadEntry(diaryEntry);
+                    break;
+                case 2:
+                    CreateEntry(diaryEntry);
+
+                    break;
+                case 3:
+                    Console.WriteLine("Avslutar programmet...");
+                    return;
+                default:
+                    Console.WriteLine("Ogiltigt val. Försök igen.");
+                    break;
+            }
+        }
+    }
+
+    ReadEntry();
+
+    CreateEntry(DateTime datum, string rubrik, string innehåll)
+    {
+        Console.WriteLine("Skriv in datum: ");
+        datum = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Skriv in en rubrik: ");
+        rubrik = Console.ReadLine();
+
+        Console.WriteLine("Skriv vad du vill lägga in i dagboken: ");
+        innehåll = Console.ReadLine();
+    }
+        }
+
